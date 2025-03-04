0c9831afca2e9bef83294a7d2265f88fd046f2b7
student35313 <s31353@pjwstk.edu.pl>
2025-03-04 22:35:08 +0100

    Add FindMaxValue method to calculate max in an array

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,10 @@
     {
         int[] numbers = { 1, 2, 3, 4, 5 };
         double average = CalculateAverage(numbers);
+        int max = FindMaxValue(numbers);
+        
         Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Maximum Value: {max}");
     }
 
     static double CalculateAverage(int[] numbers)
@@ -17,5 +20,20 @@
             sum += num;
         }
         return sum / numbers.Length;
+    }
+
+    static int FindMaxValue(int[] numbers)
+    {
+        if (numbers.Length == 0) throw new ArgumentException("Array cannot be empty");
+        
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        return max;
     }
 }
