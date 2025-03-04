08f1c9f73f5ddd4fd2e3c8f616f11f74352d40fd
student35313 <s31353@pjwstk.edu.pl>
2025-03-04 15:07:49 +0100

    Modification3

---
+++ ConsoleApp1/ConsoleApp1/Test.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp1;
+
+public class Test
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Iteration {i}");
+        }
+    }
+}
